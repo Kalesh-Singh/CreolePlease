a5638fc9cafa2866f212d0e78b077765ba07cbd3
delaneyftf <delaneyftf@gmail.com>
2017-11-09 11:24:41 -0500

    Renamed files and added support for endings conversion

--- CreolePleaseSrc.cpp
+++ CreolePleaseSrc.cpp
@@ -13,11 +13,12 @@ string ConvertStrToLower(string input);
 vector<string> StringToVectorOfWords(string input); 
 int RemovePunctuation(string &input);
 string DetermineWord(string input, unordered_map<string, vector<string>> Words); 
-vector<string> CheckMapAndReplace(vector<string> input, unordered_map<string, vector<string>> Words);
+vector<string> CheckMapAndReplaceWords(vector<string> input, unordered_map<string, vector<string>> Words);
+vector<string> CheckMapAndReplaceEndings(vector<string> input, unordered_map<string, vector<string>> Endings);
 string ReconstructString(vector<string> input); 
-string CreolePlease(string input, unordered_map<string, vector<string>> WordConversions); 
+string CreolePlease(string input, unordered_map<string, vector<string>> WordConversions, unordered_map<string, vector<string>> EndingsConversions); 
 string GetTextInputFromFile();
-void GetWordMappingsFromFile(unordered_map<string, vector<string>> &WordConversions);
+void GetMappingsFromFile(unordered_map<string, vector<string>> &Conversions, string filename);
 void WriteOutputToFile(string input);
 
 // This function converts an entire string to lowercase character by character.
@@ -43,7 +44,7 @@ vector<string> StringToVectorOfWords(string input) {
 			new_word = "";
 			continue;
 		}
-
+	
 		new_word += string(1, input[i]);
 
 		if(i == input.length()-1)
@@ -96,7 +97,7 @@ string DetermineWord(string input, unordered_map<string, vector<string>> Words) 
 // If a word with punctuation removed is found to be in the map of words, it is replaced with
 // one of its potential alternatives or left untouched. The punctuation teacker is used to append 
 // the original punctuation at the end of the word once replacements are made.
-vector<string> CheckMapAndReplace(vector<string> input, unordered_map<string, vector<string>> Words) {
+vector<string> CheckMapAndReplaceWords(vector<string> input, unordered_map<string, vector<string>> Words) {
 	vector<string>::iterator it;
 	string word = "";
 	unsigned int punctuation_tracker = 0;
@@ -121,6 +122,32 @@ vector<string> CheckMapAndReplace(vector<string> input, unordered_map<string, ve
 	return input;
 }
 
+
+// This function checks the endings of words in the vector created from the input file. If an ending match is
+// found then it is replaced with the associated value in the Endings map.
+vector<string> CheckMapAndReplaceEndings(vector<string> input, unordered_map<string, vector<string>> Endings){
+	vector<string>::iterator vec_it;
+	unordered_map<string, vector<string>>::iterator map_it;
+	int match_index = 0; 
+	
+	// For every string in the vector
+	for(vec_it = input.begin(); vec_it != input.end(); vec_it++){
+		// For every key in the map
+		for(map_it = Endings.begin(); map_it != Endings.end(); map_it++){
+			//If the key is found within the word
+			match_index = (*vec_it).find(map_it->first);
+			if(match_index != -1){
+				//Words without punctuation at the end remove last chars and add new ending.
+				if(match_index + (map_it->first).length() == (*vec_it).length()){
+					(*vec_it).erase((*vec_it).begin()+match_index, (*vec_it).end());
+					(*vec_it) += map_it->second[0];
+				}	
+			}
+		}			
+	}
+	return input;
+}
+
 // This function is used to reconstruct a string from a vector of strings it simply 
 // concatenates the strings in the vectors with spaces between. For readability on the terminal
 // a newline character is inserted approximately every 100 characters.
@@ -141,7 +168,7 @@ string ReconstructString(vector<string> input) {
 
 // This is the main function that performs a sequence of modifications and function calls on the 
 // input string to convert it to Guyanese Creolese.
-string CreolePlease(string input, unordered_map<string, vector<string>> WordConversions) {
+string CreolePlease(string input, unordered_map<string, vector<string>> WordConversions, unordered_map<string, vector<string>> EndingsConversions) {
 	string output = "";
 	vector<string> Words;
 	vector<string>::iterator it;
@@ -149,7 +176,8 @@ string CreolePlease(string input, unordered_map<string, vector<string>> WordConv
 	output = input;
 	output = ConvertStrToLower(output);
 	Words = StringToVectorOfWords(output);
-	Words = CheckMapAndReplace(Words, WordConversions);
+	Words = CheckMapAndReplaceEndings(Words, EndingsConversions); 
+	Words = CheckMapAndReplaceWords(Words, WordConversions);
 	output = ReconstructString(Words);
 	WriteOutputToFile(output);
 	return output;
@@ -157,18 +185,18 @@ string CreolePlease(string input, unordered_map<string, vector<string>> WordConv
 
 // This function reads text from an input file until the end by appending each getline to string
 // and returning the final string. 
-string GetTextInputFromFile() {
+string GetTextInputFromFile(string filename) {
 	string user_input = "";
 	string read_variable = "";
 
-	ifstream infile;
-	infile.open("infile.txt");
+	ifstream file;
+	file.open("infile.txt");
 
-	while (!infile.eof()) {
-		getline(infile, read_variable);
+	while (!file.eof()) {
+		getline(file, read_variable);
 		user_input += read_variable + " ";
 	}
-	infile.close();
+	file.close();
 	return user_input;
 }
 
@@ -176,26 +204,26 @@ string GetTextInputFromFile() {
 // and mapping via a text file. It opens the wordfile and reads lines until it encounter and eof. Each
 // line is sent toa function that converts it into a vector of words. The first element of the vector
 // is then used as a key and the remaining items are used as the values in the wordConversions map.
-void GetWordMappingsFromFile(unordered_map<string, vector<string>> &WordConversions){
-	ifstream wordfile;
-	wordfile.open("wordfile.txt");
+void GetMappingsFromFile(unordered_map<string, vector<string>> &Conversions, string filename){
+	ifstream file;
+	file.open(filename);
 
 	string current_line = "";
 	string key = "";
 	vector<string> line_vector;
 
 	while(true){
-		getline(wordfile, current_line);
+		getline(file, current_line);
 
-		if(wordfile.eof())
+		if(file.eof())
 			break;
-
+	
 		line_vector = StringToVectorOfWords(current_line);
 		key = line_vector[0];
 		line_vector.erase(line_vector.begin());
-		WordConversions[key] = line_vector;
+		Conversions[key] = line_vector;	
 	}
-	wordfile.close();
+	file.close();
 	return;
 }
 
@@ -214,12 +242,17 @@ int main()
 	//do you =-> you
 	// was not -> ain went
 	// very bad -> bad bad
+	//TODO allow stringtovector to separate punctuations
+
 	unordered_map<string, vector<string>> WordConversions;
-	GetWordMappingsFromFile(WordConversions);
+	unordered_map<string, vector<string>> EndingsConversions;
+
+	GetMappingsFromFile(WordConversions, "words.txt");
+	GetMappingsFromFile(EndingsConversions, "endings.txt");
 
 	srand(time(NULL));
 
-	string user_input = GetTextInputFromFile();
-	CreolePlease(user_input, WordConversions);
+	string user_input = GetTextInputFromFile("infile.txt");
+	CreolePlease(user_input, WordConversions, EndingsConversions);
 	return 0;
-} 
+}
